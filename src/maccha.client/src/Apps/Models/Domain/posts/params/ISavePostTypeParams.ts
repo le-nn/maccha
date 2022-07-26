b0bf576dfeafea5b79ad04ad2ae2e70f7f3b0ae3
le-nn <orenpei.okada@outlook.com>
2022-07-26 19:16:09 +0900

    Implemented frontend of category scheme setting This makes be enable to define scheme of category tree.

--- src/maccha.client/src/Apps/Models/Domain/posts/params/ISavePostTypeParams.ts
+++ src/maccha.client/src/Apps/Models/Domain/posts/params/ISavePostTypeParams.ts
@@ -1,7 +1,9 @@
+import { Category } from "../../Contents/Entities/Category";
 import { ISaveTaxonomyParams } from "../../Contents/Params";
 
 export interface ISavePostTypeParams {
     postTypeId: string;
     displayFormat: string;
     taxonomy: ISaveTaxonomyParams;
+    categories: Category[];
 }
